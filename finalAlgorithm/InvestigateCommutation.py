4b607d59fef4ed5a2dad9dbe3c161c57916745ba
jurekhackerman123 <jurek.eisinger@t-online.de>
2024-01-02 11:20:13 +0100

    a

--- finalAlgorithm/InvestigateCommutation.py
+++ finalAlgorithm/InvestigateCommutation.py
@@ -10,10 +10,7 @@ Two things will be displayed:
 '''
 
 
-def CompareDifferentArrangements(show):
-    # create the randomcircuits and the corresponding list of possible arrangements 
-    gatesList, commutationMatrix = CreateRandomCircuit(NQ, GATES, 2, display= False)
-    possibleArrangements = BFS([gatesList], commutationMatrix)
+def CompareDifferentArrangements(possibleArrangements, show):
 
     if show: 
         ShowCommutationMatrix(commutationMatrix)
@@ -57,80 +54,136 @@ def CompareDifferentArrangements(show):
     # costlist[0] corresponds to the initial element that would have been chosen if no commutation had been considered
     return [max(costList) - costList[0], len(possibleArrangements)]
 
-numberOfIterations = 500
-storingList = []
-for iteration in range(numberOfIterations): 
-    if iteration % 10 == 0:
-        print(iteration)
-    storingList.append( CompareDifferentArrangements(False) )
-
-# average improvement over iterations, considering all cases: 
-count = 0 
-for i in range(len(storingList)): 
-
-    count += storingList[i][0]
-
-print('Average improvement for all:')
-print(count/len(storingList))
-
-tabuList = []
-
-avgList = []
-indexList = []
-
-indexCount = np.zeros(50)
-
-for i in range(len(storingList)): 
-    
-    tempIndex = storingList[i][1]
-
-    print(tempIndex)
-
-
-    if tempIndex < 50: 
-        indexCount[tempIndex-1] += 1
-
-    # we only want to average over 5 values maximum for each variable 
-    if tempIndex in tabuList or tempIndex == 1:# or indexCount[tempIndex] > 5: 
-        if tempIndex == 1: 
-            indexCount[0] += 1
-        continue
-
-    tabuList.append(tempIndex)
-
-    tempCount = 0
-    tempSum = 0
 
 
 
-    for j in range(len(storingList)): 
-        if storingList[j][1] == tempIndex: 
-            tempSum += storingList[j][0]
-            tempCount += 1
-
-    avgList.append(tempSum / tempCount)
-    indexList.append(tempIndex)
-
-
-    print('Average for possiblearrangements Length ', tempIndex, ' is ', tempSum / tempCount, '. \n Where the number of averages was ', tempCount)
+def CompareTest(possibleArrangements, desiredRange, show):
+    '''
+    Assume that we just have the one arrangement without commutation -
+    What kind of variation do we have in the final cost after doing the blockaggregation a couple of times 
+    Remember: There is randomness invlved in the Block Aggregation - the placement of the idle qubits. Thus, let's see      
+    '''
 
 
-tempBins = range(1, 51)
-# print(tempBins)
 
-xValues = np.arange(50)
+    # else: 
+    #     print('Number of variations found: ', len(possibleArrangements))
+    # 
+    costList = []
 
-print(indexCount)
-plt.bar(xValues, indexCount)
-plt.xlim(-1, 10.5)
-plt.title('Histogram')
-plt.ylabel('Frequency')
-plt.xlabel('Number of possible arrangements')
-plt.show()
+    tempCircuit = possibleArrangements[0]
+    for i in range(desiredRange):
+        tempArrangement = blockProcessCircuit(tempCircuit, NQ, FSIZES, QMAX, MMAX)
 
-plt.title('Average cost improvement for different arrangements')
-plt.scatter(indexList, avgList)
-plt.xlim(0,50)
-plt.xlabel('Numbers of possible arrangements')
-plt.ylabel('Average Cost Improvement')
-plt.show()
+        costList.append(computeTotalCost(computeArrangements(tempArrangement, FSIZES, MMAX), NQ))
+
+        
+
+
+    if show == True:
+        plt.plot(costList)
+        plt.xlabel('Commutation variation No.')
+        plt.ylabel('Cost')
+        plt.title('Cost over the different possible arrangements, max diff: ' + str(max(costList) - min(costList)))
+        plt.show()
+
+    # costlist[0] corresponds to the initial element that would have been chosen if no commutation had been considered
+    return [max(costList) - costList[0], len(possibleArrangements)]
+
+
+
+def CalculatePossibleImprovement(numberOfIterations, all): 
+
+    storingList = []
+    gaugeStoringList = []
+
+    for iteration in range(numberOfIterations): 
+        if iteration % 10 == 0:
+            print(iteration)
+
+        gatesList, commutationMatrix = CreateRandomCircuit(NQ, GATES, 2, display= False)
+        possibleArrangements = BFS([gatesList], commutationMatrix)
+
+        tempResult = CompareDifferentArrangements(possibleArrangements, False)
+
+        storingList.append( tempResult )
+        gaugeStoringList.append(CompareTest(possibleArrangements, tempResult[1], False))
+
+    if all == True: 
+    # average improvement over iterations, considering all cases: 
+        count = 0 
+        for i in range(len(storingList)): 
+
+            count += storingList[i][0]
+
+        print('Average improvement for all:')
+        print(count/len(storingList))
+
+    else: 
+        tabuList = []
+
+        avgList = []
+        avgGaugeList = []
+        indexList = []
+
+        indexCount = np.zeros(50)
+
+        for i in range(len(storingList)): 
+            
+            # tempIndex is the length of the arrangement
+            tempIndex = storingList[i][1]
+
+            # we want to know the frequencies of the occurences of different ArrangementLenghts (only smaller 50 are of interest)
+            if tempIndex < 50: 
+                indexCount[tempIndex-1] += 1
+
+            # if we saw this index before or the lenght of the arrangement is one (no commutation induced rearrangement possibilities)
+            if tempIndex in tabuList or tempIndex == 1:
+                if tempIndex == 1: 
+                    indexCount[0] += 1
+                continue
+
+            tabuList.append(tempIndex)
+
+            tempCount = 0
+            tempSum = 0
+            tempGaugeSum = 0
+
+
+            for j in range(len(storingList)): 
+                if storingList[j][1] == tempIndex: 
+                    tempSum += storingList[j][0]
+
+                    tempGaugeSum += gaugeStoringList[j][0]
+                    tempCount += 1
+
+            avgList.append(tempSum / tempCount)
+            avgGaugeList.append(tempGaugeSum / tempCount)
+            indexList.append(tempIndex)
+
+
+            print('Average for possiblearrangements Length ', tempIndex, ' is ', tempSum / tempCount, '. \n Where the number of averages was ', tempCount)
+
+
+        # bar plot for the frequencies
+        xValues = np.arange(50)
+        plt.bar(xValues, indexCount)
+        plt.xlim(-1, 10.5)
+        plt.title('Histogram')
+        plt.ylabel('Frequency')
+        plt.xlabel('Number of possible arrangements')
+        plt.show()
+
+        # print average possible cost improvement for the different iterations
+        plt.title('Maximum possible cost improvement for variations')
+        plt.scatter(indexList, avgList, label = 'With commutation')
+        plt.scatter(indexList, avgGaugeList, label = 'Without commutation')
+        plt.xlim(0,50)
+        plt.xlabel('Numbers of possible arrangements')
+        plt.ylabel('Maximum possible cost improvement')
+        plt.legend()
+        plt.show()
+
+
+
+CalculatePossibleImprovement(500, all = False)
