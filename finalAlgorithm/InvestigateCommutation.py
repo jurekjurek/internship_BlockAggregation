3cb2918ad93f24b31d68354b6d4e86d35324f466
jurekhackerman123 <jurek.eisinger@t-online.de>
2024-02-03 22:48:42 +0100

    test

--- finalAlgorithm/InvestigateCommutation.py
+++ finalAlgorithm/InvestigateCommutation.py
@@ -20,141 +20,90 @@ MISSING:
 '''
 
 
-def CompareDifferentArrangements(possibleArrangements, show):
+def CompareDifferentArrangements(possibleArrangements):
+    '''
+    given a list of possible circuits, this function determines the cost of the different arrangements *relative* to the initial arrangement
+    It returns a list of relative improvements and the number of possible arrangements in this 
+    '''
+
 
     if len(possibleArrangements) == 1: 
-        # print('Only one variation found. No other arrangements due to commutation.')
         return [0, 1]
 
-    # else: 
-    #     print('Number of variations found: ', len(possibleArrangements))
-    # 
     costList = []
-    gateCoverageList = []
+
     for i in range(len(possibleArrangements)):
         tempCircuit = possibleArrangements[i]
         tempArrangement = blockProcessCircuit(tempCircuit, NQ, FSIZES, QMAX, MMAX)
 
-        # if show == True: 
-        #     visualize_blocks(tempArrangement, 'Arrangement after Block Aggregation number ' + str(i))
         costList.append(computeTotalCost(computeArrangements(tempArrangement, FSIZES, MMAX), NQ))
-
-        a, b, c, tempGateCoverage = AggregateBlocksStep(tempCircuit, NQ, QMAX, MMAX)
-        gateCoverageList.append(tempGateCoverage)
-
-        
-
-
-    if show == True:
-        plt.plot(costList)
-        plt.xlabel('Commutation variation No.')
-        plt.ylabel('Cost')
-        plt.title('Cost over the different possible arrangements, max diff: ' + str((costList[0]) - min(costList)) + ' ' + str(len(possibleArrangements)) + ' perms')
-        plt.show()
-
-        # plt.plot(gateCoverageList)
-        # plt.xlabel('Commutation variation No.')
-        # plt.ylabel('Gate Coverage')
-        # plt.title('Gate Coverage over different possible Arrangements')
-        # plt.show()
 
     # costlist[0] corresponds to the initial element that would have been chosen if no commutation had been considered
     return [((costList[0]) - min(costList)) / costList[0], len(possibleArrangements)]
 
 
-
-
-def CompareTest(possibleArrangements, desiredRange, show):
-    '''
-    Assume that we just have the one arrangement without commutation -
-    What kind of variation do we have in the final cost after doing the blockaggregation a couple of times 
-    Remember: There is randomness invlved in the Block Aggregation - the placement of the idle qubits. Thus, let's see      
-    '''
-
-
-
-    costList = []
-
-    tempCircuit = possibleArrangements[0]
-    for i in range(desiredRange):
-        tempArrangement = blockProcessCircuit(tempCircuit, NQ, FSIZES, QMAX, MMAX)
-
-        costList.append(computeTotalCost(computeArrangements(tempArrangement, FSIZES, MMAX), NQ))
-
-        
-
-
-    if show == True:
-        plt.plot(costList)
-        plt.xlabel('Commutation variation No.')
-        plt.ylabel('Cost')
-        plt.title('Cost over the different possible arrangements, max diff: ' + str(max(costList) - min(costList)))
-        plt.show()
-
-    # costlist[0] corresponds to the initial element that would have been chosen if no commutation had been considered
-    return [((costList[0]) - min(costList)) / costList[0], len(possibleArrangements)]
 
 
 
 def CalculatePossibleImprovement(numberOfIterations): 
 
-    storingList = []
-    gaugeStoringList = []
+    relativeCostList = []
+    numArrangementsList = []
 
     for iteration in range(numberOfIterations): 
         if iteration % 10 == 0:
             print('iteration No. ', iteration)
 
-        # gatesList, commutationMatrix = CreateRandomCircuit(NQ, GATES, 2, display= False)
-        # possibleArrangements = BFS([gatesList], commutationMatrix)
         randomCirc = RandomCircuit(20, 40)
         possibleArrangements = randomCirc.FindAllPossibleArrangements()
 
-        tempResult = CompareDifferentArrangements(possibleArrangements, False)
+        tempResult = CompareDifferentArrangements(possibleArrangements)
 
-        # storingList just contains all of the different max improvements
-        storingList.append( tempResult )
+
+        numArrangementsList.append(len(possibleArrangements))
+        relativeCostList.append( tempResult )
 
 
     tabuList = []
 
     avgList = []
-    indexList = []
+    
 
     indexCount = np.zeros(50)
 
-    for i in range(len(storingList)): 
+    for i in range(len(relativeCostList)): 
         
-        # tempIndex is the length of the arrangement
-        tempIndex = storingList[i][1]
+        # numArrangements is the length of the arrangement
+        numArrangements = numArrangementsList[i]
 
         # we want to know the frequencies of the occurences of different ArrangementLenghts (only smaller 50 are of interest)
-        if tempIndex < 50: 
-            indexCount[tempIndex-1] += 1
+        if numArrangements < 50: 
+            indexCount[numArrangements-1] += 1
 
         # if we saw this index before or the lenght of the arrangement is one (no commutation induced rearrangement possibilities)
-        if tempIndex in tabuList or tempIndex == 1:
-            if tempIndex == 1: 
+        if numArrangements in tabuList or numArrangements == 1:
+            if numArrangements == 1: 
                 indexCount[0] += 1
             continue
 
-        tabuList.append(tempIndex)
+        tabuList.append(numArrangements)
 
         tempCount = 0
         tempSum = 0
 
 
-        for j in range(len(storingList)): 
-            if storingList[j][1] == tempIndex: 
-                tempSum += storingList[j][0]
+        for j in range(len(relativeCostList)): 
+            if numArrangementsList[j] == numArrangements: 
+                tempSum += relativeCostList[j]
                 tempCount += 1
 
         avgList.append(tempSum / tempCount)
-        indexList.append(tempIndex)
-    print(storingList)
+        numArrangementsList.append(numArrangements)
+
+    print(relativeCostList)
 
 
-        # print('Average for possiblearrangements Length ', tempIndex, ' is ', tempSum / tempCount, '. \n Where the number of averages was ', tempCount)
+        # print('Average for possiblearrangements Length ', numArrangements, ' is ', tempSum / tempCount, '. \n Where the number of averages was ', tempCount)
 
 
     # bar plot for the frequencies
@@ -168,8 +117,8 @@ def CalculatePossibleImprovement(numberOfIterations):
 
     # print average possible cost improvement for the different iterations
     plt.title('Maximum possible cost improvement in %')
-    plt.scatter(indexList, avgList, label = 'With commutation')
-    # plt.scatter(indexList, avgGaugeList, label = 'Without commutation')
+    plt.scatter(numArrangementsList, avgList, label = 'With commutation')
+    # plt.scatter(numArrangementsList, avgGaugeList, label = 'Without commutation')
     plt.xlim(0,50)
     plt.xlabel('Numbers of possible arrangements')
     plt.ylabel('(initialcost - bestCost) / initialCost')
